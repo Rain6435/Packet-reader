b4b3392e232d240fee689f4e30d660e66b9b5190
Mohammed Elhasnaoui <77762120+Rain6435@users.noreply.github.com>
2023-03-06 13:56:45 -0500

    Update and rename main2.py to Sniffer.py

--- Sniffer.py
+++ Sniffer.py
@@ -2,8 +2,6 @@ import socket
 from scapy import all as scapy
 from scapy.all import wrpcap
 
-"""scapy.show_interfaces(True)"""
-
 def sniffing():
     s = scapy.sniff(iface='Software Loopback Interface 1',count = 30,store=True,filter="icmp")
     wrpcap("foo.pcap", s)
